87064ae7778b5d6f659d68f260ee9ee79f31ed6d
Raymond Yeh <ryeh88@gmail.com>
2016-04-07 15:09:55 -0700

    non-dynamic final

--- js/shopping-list.js
+++ js/shopping-list.js
@@ -1,10 +1,3 @@
-$(document).ready(function(){
-	// After submitting form the function will take its value and append it to the empty text field.
-	$('.itemForm').submit( function(e) {
-		var x = $("input:first").val();
-		$('.shopping-list').append(x);
-		$('.shopping-list').append("\n");
-		e.preventDefault();
-	});
-})
-
+function delete () {
+	alert('hello');
+}
