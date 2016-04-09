3613627383bca93cf6cababe9f13e6763795d5b8
Raymond Yeh <ryeh88@gmail.com>
2016-04-08 18:57:08 -0700

    New Changes

--- js/shopping-list.js
+++ js/shopping-list.js
@@ -1,3 +1,15 @@
-function delete () {
-	alert('hello');
-}
+$(document).ready( function () {
+// When enter key is pressed while in input text field
+	$('#item-add').keyup( function (e) {
+		//create a variable to store .item-add value
+		if(e.keyCode == 13) {
+			alert('hello');
+		}
+		
+	});
+
+
+
+});
+
+
