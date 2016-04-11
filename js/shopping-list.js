12adc2b8ce1e68b4769ef927bee5d778f5ba2ad7
Raymond Yeh <ryeh88@gmail.com>
2016-04-11 12:31:52 -0700

    new js

--- js/shopping-list.js
+++ js/shopping-list.js
@@ -1,15 +1,44 @@
-$(document).ready( function () {
-// When enter key is pressed while in input text field
-	$('#item-add').keyup( function (e) {
-		//create a variable to store .item-add value
-		if(e.keyCode == 13) {
-			alert('hello');
-		}
+$(document).ready(function() {
+	// Initialize form and # of items added to shopping list
+
+	// Define what happens when enter (keyCode == 13) is pressed while in the input field
+	$('.button').click( function (e) {
+	// Target Text Field
+	var textField = $('#item-name');
+	// Store user input into variable
+	var textValue = textField.val();
+
+	if (textValue.trim() == "") {
+		$('.alert').show(1000 , function(){
+			// set delay 
+			setTimeout( function() {
+				$('.alert').hide();
+			}, 1000);
+		});
 		
+		return false;
+		
+	} else {
+	// Code a <li> generator whenever the add-button or enter is pressed
+	$('#list').append('<li>' + textValue + '<button class="delete-button">X</button></li>');
+	// Clear text field after adding to shopping list
+	textField.val("");
+	// Prevents Browser from Refreshing after itms are appended
+	e.preventDefault();
+	}
+
+	//Code delete button
+	$('.delete-button').on("click", function() {
+		
+		$(this).parent().remove()
+		return false;
+	
 	});
 
+	}); 
+
+
+	
 
 
 });
-
-
